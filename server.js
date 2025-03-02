0e0c513bd2ce830c4bd67d2625175ed897a8a838
Yann Kaël ZOMENO <91330302+Kiliartis@users.noreply.github.com>
2025-03-02 17:09:42 +0100

    chore: save-my-day

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });  
+app.use((err, req, res, next) => {
+    console.error('Erreur du serveur :', err);
+    res.status(500).json({ message: 'Erreur interne du serveur' });
+  });
+
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
